7ccb8bfbc03564239f7186af766f7fec29732ab3
Piotr Justyna <piotr.justyna@gmail.com>
2020-11-30 14:15:20 +0000

    Fleshed out silo host.

---
+++ 1/SiloHost/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using Orleans.Statistics;
+
+namespace SiloHost
+{
+    class Program
+    {
+        public static Task Main()
+        {
+            var siloPort = int.Parse(Environment.GetEnvironmentVariable("SILOPORT") ?? "2000");
+            var gatewayPort = int.Parse(Environment.GetEnvironmentVariable("GATEWAYPORT") ?? "3000");
+            var advertisedIp = IPAddress.Parse(Environment.GetEnvironmentVariable("ADVERTISEDIP") ?? "192.168.0.66");
+
+            return new HostBuilder()
+                .UseOrleans(siloBuilder =>
+                {
+                    siloBuilder.UseLinuxEnvironmentStatistics();
+                    siloBuilder.UseDashboard(dashboardOptions =>
+                    {
+                        dashboardOptions.Username = "piotr";
+                        dashboardOptions.Password = "orleans";
+                    });
+                    siloBuilder.UseLocalhostClustering();
+                    siloBuilder.Configure<EndpointOptions>(endpointOptions =>
+                    {
+                        endpointOptions.AdvertisedIPAddress = advertisedIp;
+                        endpointOptions.SiloPort = siloPort;
+                        endpointOptions.GatewayPort = gatewayPort;
+                        endpointOptions.SiloListeningEndpoint = new IPEndPoint(IPAddress.Any, 2000);
+                        endpointOptions.GatewayListeningEndpoint = new IPEndPoint(IPAddress.Any, 3000);
+                    });
+                })
+                .ConfigureLogging(logging => logging.AddConsole())
+                .RunConsoleAsync();
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -1,14 +1,33 @@
+- [road to orleans](#road-to-orleans)
+  - [1](#1)
+    - [solution](#solution)
+    - [silo host](#silo-host)
+
 # road to orleans
 
 The code is written using .NET 5.
 
-## solution
+## 1
+
+This is the most basic setup: only one silo, local clustering, no clients.
+
+| clustering | clustered silos | grains | clients |
+| --- | --- | --- | --- |
+| local | 1 | 0 | 0 |
+
+### solution
 
 * `dotnet new sln -n RoadToOrleans`
 
-## silo host
+### silo host
 
 This project is going to host our grains.
 
 * `dotnet new console --language C# --name SiloHost`
 * `dotnet sln add SiloHost/SiloHost.csproj`
+* In the `SiloHost` project directory:
+    * `dotnet add package Microsoft.Orleans.OrleansRuntime --version 3.3.0`
+    * `dotnet add package Microsoft.Extensions.Hosting --version 5.0.0`
+    * `dotnet add package OrleansDashboard --version 3.1.0`
+    * `dotnet add package Microsoft.Orleans.OrleansTelemetryConsumers.Linux --version 3.3.0`
+    * `dotnet add package Microsoft.Orleans.CodeGenerator.MSBuild --version 3.3.0`
