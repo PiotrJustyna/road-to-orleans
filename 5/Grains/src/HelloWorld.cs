a846c9e637cc4e111c13940444e2c364b54bc4e6
Piotr Justyna <piotr.justyna@gmail.com>
2020-12-08 14:32:16 +0000

    First stab at solution 5.

---
+++ 5/Grains/src/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Interfaces;
+using Library;
+
+namespace Grains
+{
+    public class HelloWorld : Orleans.Grain, IHelloWorld
+    {
+        public async Task<string> SayHello(string name)
+        {
+            Say.hello("dupa");
+            return await Task.FromResult($@"Hello {name}!");
+        }
+    }
+}
