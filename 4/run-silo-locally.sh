00036e75a88b4bbf4d6ff973407196d2524c7b98
Michael Murphy <michael.murphy@emailage.com>
2020-12-10 11:10:57 +0000

    Correct port

--- 4/run-silo-locally.sh
+++ 4/run-silo-locally.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export ADVERTISEDIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
-export PEERPORT=2000
+export PEERPORT=2001
 export PEERADDRESS=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
 export GATEWAYPORT=3001,3001,3002,3003
 export SILOPORT=2001,2001,2002,2003
