f27ef315f600d0b775809a2e20c2978abdc86cda
Michael Murphy <michael.murphy@emailage.com>
2020-12-10 16:04:17 +0000

    Cleanup

--- 4/run-client-docker.sh
+++ 4/run-client-docker.sh
@@ -2,5 +2,5 @@
 ADVERTISEDIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
 GATEWAYPORT=3001
 
-docker build -t client -f ./ops/Client/Dockerfile ./ &&
-  docker run -it -e ADVERTISEDIP=$ADVERTISEDIP  -e GATEWAYPORT=$GATEWAYPORT --rm client
+docker build -t client-cluster -f ./ops/Client/Dockerfile ./ &&
+  docker run -it -e ADVERTISEDIP=$ADVERTISEDIP  -e GATEWAYPORT=$GATEWAYPORT --rm client-cluster
