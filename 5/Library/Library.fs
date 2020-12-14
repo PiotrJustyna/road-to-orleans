3633c825639a2e8a052d2167e0f5d1710c2e3dad
Mereta <mereta.degutyte@ucdconnect.ie>
2020-12-14 17:00:16 +0000

    F# Unit tests (#7)
    Unit tests for solution 5.

--- 5/Library/Library.fs
+++ 5/Library/Library.fs
@@ -2,4 +2,11 @@
 
 module Say =
     let hello name =
-        sprintf "Hello, \"%s\"! Your name is %i characters long." name (String.length name)
+        let input = 
+            match (System.String.IsNullOrWhiteSpace name) with
+            |true -> invalidArg "name" "string cannot be null or whitespace"
+            |false -> name
+        sprintf "Hello, \"%s\"! Your name is %i characters long." input (String.length input)
+
+
+        
