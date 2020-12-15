4db752def204cd88c83fd0ed52b19499b91acb33
Michael Murphy <michael.murphy@emailage.com>
2020-12-15 09:05:57 +0000

    Update name of ui environment variable

--- 4/run-silo-local.sh
+++ 4/run-silo-local.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 export ADVERTISEDIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
 export PRIMARYADDRESS=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
-export GATEWAYPORT=3003
-export UIPORT=8083
-export SILOPORT=2003
+export GATEWAYPORT=3001
+export SILOPORT=2001
 export PRIMARYPORT=2001
 
 dotnet run --project ./SiloHost/SiloHost.csproj
