59ab974ad507b1f3ca5b4197965e09ae4f01e199
Piotr Justyna <piotr.justyna@gmail.com>
2020-12-22 13:08:02 +0000

    Corrected port problems when hosting locally.

--- 5/Client/src/HelloWorldClientHostedService.cs
+++ 5/Client/src/HelloWorldClientHostedService.cs
@@ -10,15 +10,18 @@ namespace Client
     public class HelloWorldClientHostedService : IHostedService
     {
         private readonly IClusterClient _clusterClient;
+        private readonly Random _generator;
 
         public HelloWorldClientHostedService(IClusterClient clusterClient)
         {
             _clusterClient = clusterClient;
+            _generator = new Random();
         }
 
         public async Task StartAsync(CancellationToken cancellationToken)
         {
-            var helloWorldGrain = _clusterClient.GetGrain<IHelloWorld>(0);
+            // A random integer is generated to allow for a new hello world grain to be created per client creation.
+            var helloWorldGrain = _clusterClient.GetGrain<IHelloWorld>(_generator.Next(int.MaxValue));
             Console.WriteLine($"{await helloWorldGrain.SayHello("Piotr")}");
         }
 
