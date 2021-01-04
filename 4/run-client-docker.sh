926d8d63bc6e50b5ca5fb1f926a3588eade17672
Michael Murphy <michael.murphy@emailage.com>
2021-01-04 10:47:07 +0000

    Break solutions into branches

--- 4/run-client-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ADVERTISEDIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v -m1 '127.0.0.1'`
-GATEWAYPORT=3001
-
-docker build -t client-cluster -f ./ops/Client/Dockerfile ./ &&
-  docker run -it -e ADVERTISEDIP=$ADVERTISEDIP  -e GATEWAYPORT=$GATEWAYPORT --rm client-cluster
--- 6/run-demo-cluster.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-./ops/Convenience/run-silo-1-docker.sh &
-./ops/Convenience/run-silo-3-docker.sh &
-./ops/Convenience/run-silo-2-local.sh
