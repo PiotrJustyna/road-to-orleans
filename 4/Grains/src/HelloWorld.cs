4a7a2d756aec81b034e406f5552c08cdce41409a
Michael Murphy <5879863+mrblonde91@users.noreply.github.com>
2021-01-08 11:12:01 +0000

    Add solution 4

---
+++ 4/Grains/src/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Interfaces;
+
+namespace Grains
+{
+    public class HelloWorld : Orleans.Grain, IHelloWorld
+    {
+        public async Task<string> SayHello(string name)
+        {
+            return await Task.FromResult($@"Hello {name}!");
+        }
+    }
+}
