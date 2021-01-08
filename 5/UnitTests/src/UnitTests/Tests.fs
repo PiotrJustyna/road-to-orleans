1b458112d5a72b96b188c59dc745acd2fb6e6938
Michael Murphy <5879863+mrblonde91@users.noreply.github.com>
2021-01-08 11:12:22 +0000

    Add solution 5

---
+++ 5/UnitTests/src/UnitTests/Tests.fs
@@ -0,0 +1,25 @@
+module UnitTests.Tests
+
+open Xunit
+open Library
+
+[<Fact>]
+let ``Library Valid String Input Test`` () =
+    let result = Say.hello("Mereta")
+    Assert.Equal("Hello, \"Mereta\"! Your name is 6 characters long.", result)
+
+[<Fact>]
+let ``Library Empty String Input Throws Exception`` () =
+    Assert.Throws<System.ArgumentException>((fun () -> Say.hello "" |> ignore))
+
+[<Fact>]
+let ``Library Whitespace String Input Throws Exception`` () =
+    Assert.Throws<System.ArgumentException>((fun () -> Say.hello "\t" |> ignore))
+
+[<Fact>]
+let ``Library Exception Messgage Test`` () = 
+    let ex = Assert.Throws<System.ArgumentException>((fun () -> Say.hello "" |> ignore));
+    Assert.Equal("string cannot be null or whitespace (Parameter 'name')" , ex.Message);
+    Assert.Same(typeof<System.ArgumentException>, ex.GetType())
+
+
