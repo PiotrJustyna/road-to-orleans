febdbc4cc19b9129e9270a5c7d5f9373bf7c66d8
Michael Murphy <5879863+mrblonde91@users.noreply.github.com>
2021-01-08 10:58:35 +0000

    Add solution 1

---
+++ 1/SiloHost/src/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using Orleans.Statistics;
+
+namespace SiloHost
+{
+    class Program
+    {
+        public static Task Main()
+        {
+            var siloEndpointConfiguration = GetSiloEndpointConfiguration();
+            
+            return new HostBuilder()
+                .UseOrleans(siloBuilder =>
+                {
+                    siloBuilder.UseLinuxEnvironmentStatistics();
+                    siloBuilder.UseDashboard(dashboardOptions =>
+                    {
+                        dashboardOptions.Username = "piotr";
+                        dashboardOptions.Password = "orleans";
+                    });
+                    siloBuilder.UseLocalhostClustering();
+                    siloBuilder.Configure<EndpointOptions>(endpointOptions =>
+                    {
+                        endpointOptions.AdvertisedIPAddress = siloEndpointConfiguration.Ip;
+                        endpointOptions.SiloPort = siloEndpointConfiguration.SiloPort;
+                        endpointOptions.GatewayPort = siloEndpointConfiguration.GatewayPort;
+                        endpointOptions.SiloListeningEndpoint = new IPEndPoint(IPAddress.Any, 2000);
+                        endpointOptions.GatewayListeningEndpoint = new IPEndPoint(IPAddress.Any, 3000);
+                    });
+                })
+                .ConfigureLogging(logging => logging.AddConsole())
+                .RunConsoleAsync();
+        }
+        
+        private static SiloEndpointConfiguration GetSiloEndpointConfiguration()
+        {
+            return new SiloEndpointConfiguration(
+                GetLocalIpAddress(),
+                2000,
+                3000);
+        }
+
+        private static IPAddress GetLocalIpAddress()
+        {
+            var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var network in networkInterfaces)
+            {
+                if (network.OperationalStatus != OperationalStatus.Up)
+                    continue;
+
+                var properties = network.GetIPProperties();
+                if (properties.GatewayAddresses.Count == 0)
+                    continue;
+
+                foreach (var address in properties.UnicastAddresses)
+                {
+                    if (address.Address.AddressFamily == AddressFamily.InterNetwork &&
+                        !IPAddress.IsLoopback(address.Address))
+                    {
+                        return address.Address;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
