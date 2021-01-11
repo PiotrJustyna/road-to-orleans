ebb24ac83cb6d8ea47026ad7876806a5178412f5
Michael Murphy <5879863+mrblonde91@users.noreply.github.com>
2021-01-11 11:26:09 +0000

    Add dynamo connection and configuration in docker files

---
+++ 8/Interfaces/src/IHelloWorld.cs
@@ -0,0 +1,10 @@
+﻿using Orleans;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface IHelloWorld : IGrainWithIntegerKey
+    {
+        Task<string> SayHello(string name, GrainCancellationToken grainCancellationToken);
+    }
+}
