149f1a696c18911a56bf4fbcedff37694b43853c
Michael Murphy <5879863+mrblonde91@users.noreply.github.com>
2021-01-13 16:01:21 +0000

    Cleanup file

--- 8/SiloHost/src/SiloEndpointConfiguration.cs
+++ 8/SiloHost/src/SiloEndpointConfiguration.cs
@@ -16,29 +16,5 @@ namespace SiloHost
             int siloPort,
             int gatewayPort,
             int dashboardPort) => (Ip, SiloPort, GatewayPort, DashboardPort) = (ip, siloPort, gatewayPort, dashboardPort);
-        
-        public SiloEndpointConfiguration(
-            string ip,
-            int siloPort,
-            int gatewayPort)
-        {
-            if (IPAddress.TryParse(ip, out var parsedIp))
-            {
-                Ip = parsedIp;
-            }
-            
-            SiloPort = siloPort;
-            GatewayPort = gatewayPort;
-        }
-        
-        public SiloEndpointConfiguration(
-            IPAddress ip,
-            int siloPort,
-            int gatewayPort,
-            int dashboardPort,
-            int siloListeningPort,
-            int gatewayListeningPort) =>
-            (Ip, SiloPort, GatewayPort, DashboardPort, SiloListeningPort, GatewayListeningPort) = (ip, siloPort,
-                gatewayPort, dashboardPort, siloListeningPort, gatewayListeningPort);
     }
 }
