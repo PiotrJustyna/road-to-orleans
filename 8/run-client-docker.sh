6bed1c4a15c30d9c1d38778a80824bf7b4ce6d20
Michael Murphy <5879863+mrblonde91@users.noreply.github.com>
2021-01-14 10:46:18 +0000

    Update folder structure and moved libraries

--- 8/run-client-docker.sh
+++ 8/run-client-docker.sh
@@ -4,8 +4,8 @@
 # Code below gets local IP for dockerized applications to function and communicate correctly.
 ADVERTISEDIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v -m1 '127.0.0.1'`
 GATEWAYPORT=3001
-MEMBERSHIPTABLE="OrleansMembership"
-AWSREGION="us-west-2"
+MEMBERSHIPTABLE=OrleansMembership
+AWSREGION=http://$ADVERTISEDIP:8042
 
 docker build -t client-cluster -f ./ops/Api/Dockerfile ./ &&
   docker run -it -p 5432:80 -e ADVERTISEDIP=$ADVERTISEDIP  -e GATEWAYPORT=$GATEWAYPORT -e MEMBERSHIPTABLE=$MEMBERSHIPTABLE -e AWSREGION=$AWSREGION --rm client-cluster 
