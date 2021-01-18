81e01065a07ab6eaa50508f35950741218dbbad8
Michael Murphy <5879863+mrblonde91@users.noreply.github.com>
2021-01-18 16:21:39 +0000

    Add credentials

--- 8/run-client-docker.sh
+++ 8/run-client-docker.sh
@@ -8,4 +8,5 @@ MEMBERSHIPTABLE="test-orleans-table"
 #Can also optionally setup a local dynamo db and point at it like so http://$ADVERTISEDIP:8042
 AWSREGION="us-west-2"
 docker build -t client-cluster -f ./ops/Api/Dockerfile ./ &&
-  docker run -it -p 5432:80 -e MEMBERSHIPTABLE=$MEMBERSHIPTABLE -e AWSREGION=$AWSREGION --rm client-cluster 
+  docker run -it -p 5432:80 -e AWS_REGION -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_ACCESS_KEY_ID\
+   -e MEMBERSHIPTABLE=$MEMBERSHIPTABLE -e AWSREGION=$AWSREGION --rm client-cluster 
