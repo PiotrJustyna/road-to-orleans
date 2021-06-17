ae4888e3843386566b88713dbab0ac48e3c04a49
Piotr Justyna <piotr.justyna@gmail.com>
2021-06-17 12:06:52 +0100

    Minor docker compose cleanup.

--- 3b/run-silo-docker.sh
+++ 3b/run-silo-docker.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+
+
 # 2020-12-07 PJ:
 # Code below gets local IP for dockerized applications to function and communicate correctly.
-echo ADVERTISEDIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'` >> ./ops/SiloHost/dd.env
+ADVERTISEDIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+
+echo "ADVERTISEDIP=$ADVERTISEDIP" > ./ops/SiloHost/dd.env
 
 docker build -t silo-host -f ./ops/SiloHost/Dockerfile ./ &&
     docker-compose --env-file ./ops/SiloHost/dd.env -f ./ops/SiloHost/docker-compose.yml up
