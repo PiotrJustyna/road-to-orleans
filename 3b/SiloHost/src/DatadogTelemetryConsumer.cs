822f8505cd838fd43a3fe1d7e97339057ff52f9e
Piotr Justyna <piotr.justyna@gmail.com>
2021-06-17 16:04:11 +0100

    Getting closer to a working solution 3b.

--- 3b/SiloHost/src/DatadogTelemetryConsumer.cs
+++ 3b/SiloHost/src/DatadogTelemetryConsumer.cs
@@ -10,88 +10,27 @@ namespace SiloHost
     {
         private readonly DogStatsdService _service;
 
-        private readonly string[] _metricPrefixes = {"App.Requests.", "Grain."};
-
         public DatadogTelemetryConsumer()
         {
-            var config = new StatsdConfig();
+            var statsdConfig = new StatsdConfig();
+
             _service = new DogStatsdService();
-            _service.Configure(config);
+            _service.Configure(statsdConfig);
         }
 
-        public void DecrementMetric(string name)
-        {
-            if (ShouldRecord(name))
-            {
-                _service.Decrement(FormatMetricName(name));
-            }
-        }
+        public void DecrementMetric(string name) => _service.Decrement(name);
 
-        public void DecrementMetric(
-            string name,
-            double value)
-        {
-            if (ShouldRecord(name))
-            {
-                _service.Decrement(
-                    FormatMetricName(name),
-                    (int) Math.Clamp(
-                        value,
-                        int.MinValue,
-                        int.MaxValue));
-            }
-        }
+        public void DecrementMetric(string name, double value) => _service.Decrement(name, Clamp(value));
 
-        public void IncrementMetric(string name)
-        {
-            if (ShouldRecord(name))
-            {
-                _service.Increment(FormatMetricName(name));
-            }
-        }
+        public void IncrementMetric(string name) => _service.Increment(name);
 
-        public void IncrementMetric(
-            string name,
-            double value)
-        {
-            if (ShouldRecord(name))
-            {
-                _service.Increment(
-                    FormatMetricName(name),
-                    (int) Math.Clamp(
-                        value,
-                        int.MinValue,
-                        int.MaxValue));
-            }
-        }
+        public void IncrementMetric(string name, double value) => _service.Increment(name, Clamp(value));
 
-        public void TrackMetric(
-            string name,
-            TimeSpan value,
-            IDictionary<string, string> properties = null)
-        {
-            if (ShouldRecord(name))
-            {
-                _service.Distribution(
-                    FormatMetricName(name),
-                    value.TotalMilliseconds,
-                    tags: GetTags(properties)?.ToArray());
-            }
-        }
+        public void TrackMetric(string name, TimeSpan value, IDictionary<string, string> properties = null) =>
+            _service.Distribution(name, value.TotalMilliseconds, tags: ToTags(properties));
 
-        public void TrackMetric(
-            string name,
-            double value,
-            IDictionary<string, string> properties = null)
-        {
-            if (ShouldRecord(name))
-            {
-                _service.Distribution(
-                    FormatMetricName(name),
-                    value,
-                    tags: GetTags(properties)?.ToArray());
-            }
-        }
+        public void TrackMetric(string name, double value, IDictionary<string, string> properties = null) =>
+            _service.Distribution(name, value, tags: ToTags(properties));
 
         public void Flush() => _service.Flush();
 
@@ -99,17 +38,10 @@ namespace SiloHost
 
         public void Dispose() => _service.Dispose();
 
-        // private bool ShouldRecord(string name) => _metricPrefixes.Any(name.StartsWith);
+        private int Clamp(double value) => (int) Math.Clamp(value, int.MinValue, int.MaxValue);
 
-        private bool ShouldRecord(string name) => true;
+        private string[] ToTags(IDictionary<string, string> properties) => properties?.Select(ToTag).ToArray();
 
-        private string FormatMetricName(string name) => $"orleans.{name}";
-
-        private IEnumerable<string> GetTags(IDictionary<string, string> properties)
-        {
-            return properties?
-                .Select(kvp =>
-                    $"{kvp.Key.Replace(':', '_')}:{kvp.Value.Replace(':', '_')}");
-        }
+        private static string ToTag(KeyValuePair<string, string> property) => $"{property.Key}:{property.Value}";
     }
 }
--- 3b/run-silo-docker.sh
+++ 3b/run-silo-docker.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-
-
 # 2020-12-07 PJ:
 # Code below gets local IP for dockerized applications to function and communicate correctly.
 ADVERTISEDIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+GATEWAYPORT='3000'
+DASHBOARDPORT='8080'
+DATADOG_SERVICE_NAME='road-to-orleans-3b'
+DATADOG_ENVIRONMENT='dev'
+DATADOG_API_KEY='API_KEY_HERE'
+ENVIRONMENT_VARIABLES_FILE="./ops/SiloHost/$DATADOG_ENVIRONMENT.env"
 
-echo "ADVERTISEDIP=$ADVERTISEDIP" > ./ops/SiloHost/dd.env
+echo "ADVERTISEDIP=$ADVERTISEDIP
+DATADOG_SERVICE_NAME=$DATADOG_SERVICE_NAME
+DATADOG_ENVIRONMENT=$DATADOG_ENVIRONMENT
+DATADOG_API_KEY=$DATADOG_API_KEY
+GATEWAYPORT=$GATEWAYPORT
+DASHBOARDPORT=$DASHBOARDPORT" > $ENVIRONMENT_VARIABLES_FILE
 
 docker build -t silo-host -f ./ops/SiloHost/Dockerfile ./ &&
-    docker-compose --env-file ./ops/SiloHost/dd.env -f ./ops/SiloHost/docker-compose.yml up
+    docker-compose --env-file $ENVIRONMENT_VARIABLES_FILE -f ./ops/SiloHost/docker-compose.yml up
