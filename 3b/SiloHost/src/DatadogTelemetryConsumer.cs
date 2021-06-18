a8b4f54e9f5056943d0d6527d036f99fc6ec11d6
Piotr Justyna <piotr.justyna@gmail.com>
2021-06-18 15:05:43 +0100

    Solution 3b finished.

--- 3b/SiloHost/src/DatadogTelemetryConsumer.cs
+++ 3b/SiloHost/src/DatadogTelemetryConsumer.cs
@@ -6,42 +6,42 @@ using StatsdClient;
 
 namespace SiloHost
 {
-    public class DatadogTelemetryConsumer : IMetricTelemetryConsumer, IDisposable
+public class DatadogTelemetryConsumer : IMetricTelemetryConsumer, IDisposable
+{
+    private readonly DogStatsdService _service;
+
+    public DatadogTelemetryConsumer()
     {
-        private readonly DogStatsdService _service;
+        var statsdConfig = new StatsdConfig();
 
-        public DatadogTelemetryConsumer()
-        {
-            var statsdConfig = new StatsdConfig();
-
-            _service = new DogStatsdService();
-            _service.Configure(statsdConfig);
-        }
-
-        public void DecrementMetric(string name) => _service.Decrement(name);
-
-        public void DecrementMetric(string name, double value) => _service.Decrement(name, Clamp(value));
-
-        public void IncrementMetric(string name) => _service.Increment(name);
-
-        public void IncrementMetric(string name, double value) => _service.Increment(name, Clamp(value));
-
-        public void TrackMetric(string name, TimeSpan value, IDictionary<string, string> properties = null) =>
-            _service.Distribution(name, value.TotalMilliseconds, tags: ToTags(properties));
-
-        public void TrackMetric(string name, double value, IDictionary<string, string> properties = null) =>
-            _service.Distribution(name, value, tags: ToTags(properties));
-
-        public void Flush() => _service.Flush();
-
-        public void Close() => _service.Flush();
-
-        public void Dispose() => _service.Dispose();
-
-        private int Clamp(double value) => (int) Math.Clamp(value, int.MinValue, int.MaxValue);
-
-        private string[] ToTags(IDictionary<string, string> properties) => properties?.Select(ToTag).ToArray();
-
-        private static string ToTag(KeyValuePair<string, string> property) => $"{property.Key.Replace(':', '_')}:{property.Value.Replace(':', '_')}";
+        _service = new DogStatsdService();
+        _service.Configure(statsdConfig);
     }
+
+    public void DecrementMetric(string name) => _service.Decrement(name);
+
+    public void DecrementMetric(string name, double value) => _service.Decrement(name, Clamp(value));
+
+    public void IncrementMetric(string name) => _service.Increment(name);
+
+    public void IncrementMetric(string name, double value) => _service.Increment(name, Clamp(value));
+
+    public void TrackMetric(string name, TimeSpan value, IDictionary<string, string> properties = null) =>
+        _service.Distribution(name, value.TotalMilliseconds, tags: ToTags(properties));
+
+    public void TrackMetric(string name, double value, IDictionary<string, string> properties = null) =>
+        _service.Distribution(name, value, tags: ToTags(properties));
+
+    public void Flush() => _service.Flush();
+
+    public void Close() => _service.Flush();
+
+    public void Dispose() => _service.Dispose();
+
+    private int Clamp(double value) => (int) Math.Clamp(value, int.MinValue, int.MaxValue);
+
+    private string[] ToTags(IDictionary<string, string> properties) => properties?.Select(ToTag).ToArray();
+
+    private static string ToTag(KeyValuePair<string, string> property) => $"{property.Key.Replace(':', '_')}:{property.Value.Replace(':', '_')}";
+}
 }
--- 3b/readme.md
+++ 3b/readme.md
@@ -1,9 +1,41 @@
 # readme
 
-Run silo host 
-1. Add your DD api key on docker-compose 
-2. run run-silo-docker.sh
+This example builds on ttop of Solution 3 by introducing datadog as an APM tool. The goal is to demonstrate how:
 
-1. all metrics are tracked - if you don't want to track everything, filter out the unwanted metrics
-2. screenshots of metrics explorer
-3. screenshot of metrics summary - https://app.datadoghq.com/metric/summary
+* send orleans metrics to datadog
+* implement `IMetricTelemetryConsumer` with datadog
+
+## how it works
+
+In addition to docker images defined in solution 3, we now have one more, which is hosting the datadog agent. The way datadog works is that it needs:
+
+* tracer - this runs alongside the executable code it's supposed to monitor (in the same container)
+* agent - this runs as a separate container alongside the container hosting your monitored application
+
+We now have a [docker-compose.yml](./ops/SiloHost/docker-compose.yml) file which starts the datadog agent & silo host  duo.
+
+## running the code
+
+The easiest way to run the code is to execute:
+
+* run-silo-docker.sh - this executes the [docker-compose.yml](./ops/SiloHost/docker-compose.yml) file and builds not only the silo host, but agent container propagating the data to datadog
+  ![](./img/docker-compose-output.png)
+* run-client-docker.sh - this starts the orleans client, no changes there
+
+## metrics
+
+There are 167 metrics the telemetry consumer exposes:
+
+![all metrics](./img/metrics-summary.png)
+
+They are all correctly tagged:
+
+![metric tags](./img/tagged-metric.png)
+
+Captured metrics help us formulate good understanding of what our orleans application is doing and what the resource consumption is. To list some of the most useful metrics:
+
+|||
+| --- | --- |
+| `Grain.Grains.HelloWorld.Current` - number of `HelloWorld` grain. In our case it's an uneventful one grain. ![](./img/Grain.Grains.HelloWorld.Current.png) | `App.Requests.Latency.Average.Millis.Current` - average application latency. ![](./img/App.Requests.Latency.Average.Millis.Current.png) |
+| `App.Requests.Total.Requests.Current` - total number of requests an application receives. In this example the application was restarted which reset the counter to 0. ![](./img/App.Requests.Total.Requests.Current.png) | `Runtime.Memory.AvailableMemoryMb.Current` - amount of available memory in Mb. ![](./img/Runtime.Memory.AvailableMemoryMb.Current.png) |
+| `Runtime.CpuUsage.Current` - amount of CPU the runtime consumes. ![](./img/Runtime.CpuUsage.Current.png) | `App.Requests.TimedOut.Current` - number of timed out requests. Uneventful zero. ![](./img/App.Requests.TimedOut.Current.png) |
