f99193a51c79f436ef3b545dfaeec559cf7699d6
Michele Scandura <michele.scandura@lexisnexisrisk.com>
2021-10-19 15:42:57 +0100

    Add -m1 in the last grep to fetch the first result

--- 3a/run-silo-docker.sh
+++ 3a/run-silo-docker.sh
@@ -2,7 +2,7 @@
 
 # 2020-12-07 PJ:
 # Code below gets local IP for dockerized applications to function and communicate correctly.
-ADVERTISEDIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+ADVERTISEDIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v -m1 '127.0.0.1'`
 GATEWAYPORT=3000
 
 docker build -t silo-host -f ./ops/SiloHost/Dockerfile ./ &&
