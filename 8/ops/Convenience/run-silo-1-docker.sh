eb592b4a89872280b825c89fccf58d0154bedb44
Bogdan Babiy <bogg77babiy@gmail.com>
2021-10-21 16:19:05 +0100

    Fixed environment variable names when running a silo cluster

--- 8/ops/Convenience/run-silo-1-docker.sh
+++ 8/ops/Convenience/run-silo-1-docker.sh
@@ -3,15 +3,20 @@ RetrieveIp(){
   ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v -m1 '127.0.0.1'
 }
 
-ADVERTISEDIP=`RetrieveIp`
-GATEWAYPORT=3001
-DASHBOARDPORT=8081
-SILOPORT=2001
-MEMBERSHIPTABLE="test-orleans-table"
-AWSREGION="us-west-2"
-ISLOCAL=true
+OrleansHostSettings__AdvertisedIp=`RetrieveIp`
+OrleansHostSettings__GatewayPort=3001
+OrleansHostSettings__DashboardPort=8081
+OrleansHostSettings__SiloPort=2001
+OrleansHostSettings__MembershipTableName="some-table-name"
+OrleansHostSettings__AwsRegion="us-west-2"
+OrleansHostSettings__IsLocal=true
+
+AWS_REGION=$(awk -F '=' '/^region/ { print $2 }' ~/.aws/credentials)
+AWS_SECRET_ACCESS_KEY=$(awk -F '=' '/^aws_secret_access_key/ { print $2 }' ~/.aws/credentials)
+AWS_SESSION_TOKEN=$(awk -F '=' '/^aws_session_token/ { print $2 }' ~/.aws/credentials)
+AWS_ACCESS_KEY_ID=$(awk -F '=' '/^aws_access_key_id/ { print $2 }' ~/.aws/credentials)
 
 docker build -t silo-host-cluster -f ./ops/SiloHost/Dockerfile ./ &&
-  docker run -d -e AWS_REGION -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_ACCESS_KEY_ID\
-  -e ADVERTISEDIP=$ADVERTISEDIP  -e GATEWAYPORT=$GATEWAYPORT -e SILOPORT=$SILOPORT -e DASHBOARDPORT=$DASHBOARDPORT\
-   -e ISLOCAL=$ISLOCAL -p $GATEWAYPORT:$GATEWAYPORT -p $SILOPORT:$SILOPORT -p $DASHBOARDPORT:$DASHBOARDPORT  -e MEMBERSHIPTABLE=$MEMBERSHIPTABLE -e AWSREGION=$AWSREGION --rm silo-host-cluster
+  docker run -e AWS_REGION=$AWS_REGION -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID\
+  -e OrleansHostSettings__AdvertisedIp=$OrleansHostSettings__AdvertisedIp  -e OrleansHostSettings__GatewayPort=$OrleansHostSettings__GatewayPort -e OrleansHostSettings__SiloPort=$OrleansHostSettings__SiloPort -e OrleansHostSettings__DashboardPort=$OrleansHostSettings__DashboardPort\
+   -e OrleansHostSettings__IsLocal=$OrleansHostSettings__IsLocal -p $OrleansHostSettings__GatewayPort:$OrleansHostSettings__GatewayPort -p $OrleansHostSettings__SiloPort:$OrleansHostSettings__SiloPort -p $OrleansHostSettings__DashboardPort:$OrleansHostSettings__DashboardPort  -e OrleansHostSettings__MembershipTableName=$OrleansHostSettings__MembershipTableName -e OrleansHostSettings__AwsRegion=$OrleansHostSettings__AwsRegion --rm silo-host-cluster
