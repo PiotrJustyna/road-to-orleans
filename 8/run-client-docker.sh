224895590577319fab956c6390305fc53bf4c833
Bogdan Babiy <bogg77babiy@gmail.com>
2021-10-26 12:01:51 +0100

    Fixed ENV variables in bash scripts

--- 8/run-client-docker.sh
+++ 8/run-client-docker.sh
@@ -4,10 +4,14 @@
 # Code below gets local IP for dockerized applications to function and communicate correctly.
 ADVERTISEDIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v -m1 '127.0.0.1'`
 GATEWAYPORT=3001
-MEMBERSHIPTABLE="test-orleans-table"
-#Can also optionally setup a local dynamo db and point at it like so http://$ADVERTISEDIP:8042
-AWSREGION="us-west-2"
+OrleansSettings__MembershipTable="some-table-name"
+OrleansSettings__AwsRegion="us-west-2"
 
-docker build -t client-cluster -f ./ops/Api/Dockerfile ./ &&
-  docker run -it -p 5432:80 -e AWS_REGION -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_ACCESS_KEY_ID \
-   -e MEMBERSHIPTABLE=$MEMBERSHIPTABLE -e AWSREGION=$AWSREGION --rm client-cluster 
+AWS_REGION=$(awk -F '=' '/^region/ { print $2 }' ~/.aws/credentials)
+AWS_SECRET_ACCESS_KEY=$(awk -F '=' '/^aws_secret_access_key/ { print $2 }' ~/.aws/credentials)
+AWS_SESSION_TOKEN=$(awk -F '=' '/^aws_session_token/ { print $2 }' ~/.aws/credentials)
+AWS_ACCESS_KEY_ID=$(awk -F '=' '/^aws_access_key_id/ { print $2 }' ~/.aws/credentials)
+
+docker build -t client-cluster -f ./ops/Api/Dockerfile ./ 
+  docker run -it -p 5432:80 -e AWS_REGION -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID  \
+   -e OrleansSettings__MembershipTable=$OrleansSettings__MembershipTable -e OrleansSettings__AwsRegion=$OrleansSettings__AwsRegion --rm client-cluster 
--- 8/run-client-local.sh
+++ 8/run-client-local.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-export ISLOCAL=true
-export MEMBERSHIPTABLE="test-orleans-table"
-export AWSREGION="us-west-2"
-export CLUSTER_ID="cluster-of-silos"
+export OrleansSettings__IsLocal=true
+export OrleansSettings__MembershipTable="some-table-name"
+export OrleansSettings__AwsRegion="us-west-2"
 
-dotnet run --project ./Api/Api.csproj
+AWS_REGION=$(awk -F '=' '/^region/ { print $2 }' ~/.aws/credentials)
+AWS_SECRET_ACCESS_KEY=$(awk -F '=' '/^aws_secret_access_key/ { print $2 }' ~/.aws/credentials)
+AWS_SESSION_TOKEN=$(awk -F '=' '/^aws_session_token/ { print $2 }' ~/.aws/credentials)
+AWS_ACCESS_KEY_ID=$(awk -F '=' '/^aws_access_key_id/ { print $2 }' ~/.aws/credentials)
+
+dotnet run --no-launch-profile --project ./Api/Api.csproj
