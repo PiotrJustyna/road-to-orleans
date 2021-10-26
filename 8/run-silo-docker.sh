650da93ae2a2907dca05596d69e4668bafad0c75
Bogdan Babiy <bogg77babiy@gmail.com>
2021-10-26 12:01:51 +0100

    Fixed ENV variables in bash scripts

--- 8/run-silo-docker.sh
+++ 8/run-silo-docker.sh
@@ -3,16 +3,23 @@ RetrieveIp(){
   ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v -m1 '127.0.0.1'
 }
 
-ADVERTISEDIP=`RetrieveIp`
-GATEWAYPORT=3001
-SILOPORT=2001
-DASHBOARDPORT=8081
-MEMBERSHIPTABLE="test-orleans-table"
+OrleansHostSettings__AdvertisedIp=`RetrieveIp`
+OrleansHostSettings__GatewayPort=3001
+OrleansHostSettings__DashboardPort=8081
+OrleansHostSettings__SiloPort=2001
+OrleansHostSettings__MembershipTableName="some-table-name"
 #Can also optionally setup a local dynamo db and point at it like so http://$ADVERTISEDIP:8042
-AWSREGION="us-west-2"
-ISLOCAL=true
+OrleansHostSettings__AwsRegion="us-west-2"
+OrleansHostSettings__IsLocal=true
+
+AWS_REGION=$(awk -F '=' '/^region/ { print $2 }' ~/.aws/credentials)
+AWS_SECRET_ACCESS_KEY=$(awk -F '=' '/^aws_secret_access_key/ { print $2 }' ~/.aws/credentials)
+AWS_SESSION_TOKEN=$(awk -F '=' '/^aws_session_token/ { print $2 }' ~/.aws/credentials)
+AWS_ACCESS_KEY_ID=$(awk -F '=' '/^aws_access_key_id/ { print $2 }' ~/.aws/credentials)
+
+
 
 docker build -t silo-host-cluster -f ./ops/SiloHost/Dockerfile ./ &&
-  docker run -it -e AWS_REGION -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_ACCESS_KEY_ID\
-   -e ADVERTISEDIP=$ADVERTISEDIP -e ISLOCAL=$ISLOCAL  -e GATEWAYPORT=$GATEWAYPORT -e SILOPORT=$SILOPORT -e PRIMARYPORT=$PRIMARYPORT -e DASHBOARDPORT=$DASHBOARDPORT\
-   -e PRIMARYADDRESS=$PRIMARYADDRESS -e MEMBERSHIPTABLE=$MEMBERSHIPTABLE -e AWSREGION=$AWSREGION  -p $GATEWAYPORT:$GATEWAYPORT -p $SILOPORT:$SILOPORT -p $DASHBOARDPORT:$DASHBOARDPORT --rm silo-host-cluster
+  docker run -it -e AWS_REGION=$AWS_REGION -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID\
+  -e OrleansHostSettings__AdvertisedIp=$OrleansHostSettings__AdvertisedIp  -e OrleansHostSettings__GatewayPort=$OrleansHostSettings__GatewayPort -e OrleansHostSettings__SiloPort=$OrleansHostSettings__SiloPort -e OrleansHostSettings__DashboardPort=$OrleansHostSettings__DashboardPort\
+   -e OrleansHostSettings__IsLocal=$OrleansHostSettings__IsLocal -p $OrleansHostSettings__GatewayPort:$OrleansHostSettings__GatewayPort -p $OrleansHostSettings__SiloPort:$OrleansHostSettings__SiloPort -p $OrleansHostSettings__DashboardPort:$OrleansHostSettings__DashboardPort  -e OrleansHostSettings__MembershipTableName=$OrleansHostSettings__MembershipTableName -e OrleansHostSettings__AwsRegion=$OrleansHostSettings__AwsRegion --rm silo-host-cluster
--- 8/run-silo-local.sh
+++ 8/run-silo-local.sh
@@ -3,13 +3,17 @@ RetrieveIp(){
   ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v -m1 '127.0.0.1'
 }
 
-export ADVERTISEDIP=`RetrieveIp`
-export GATEWAYPORT=3001
-export SILOPORT=2001
-export DASHBOARDPORT=8081
-export ISLOCAL=true
-export MEMBERSHIPTABLE="test-orleans-table"
-export AWSREGION="us-west-2"
-export CLUSTER_ID="cluster-of-silos"
+export OrleansHostSettings__AdvertisedIp=`RetrieveIp`
+export OrleansHostSettings__GatewayPort=3001
+export OrleansHostSettings__DashboardPort=8081
+export OrleansHostSettings__SiloPort=2001
+export OrleansHostSettings__MembershipTableName="some-table-name"
+export OrleansHostSettings__AwsRegion="us-west-2"
+export OrleansHostSettings__IsLocal=true
 
-dotnet run --project ./SiloHost/SiloHost.csproj
+export AWS_REGION=$(awk -F '=' '/^region/ { print $2 }' ~/.aws/credentials)
+export AWS_SECRET_ACCESS_KEY=$(awk -F '=' '/^aws_secret_access_key/ { print $2 }' ~/.aws/credentials)
+export AWS_SESSION_TOKEN=$(awk -F '=' '/^aws_session_token/ { print $2 }' ~/.aws/credentials)
+export AWS_ACCESS_KEY_ID=$(awk -F '=' '/^aws_access_key_id/ { print $2 }' ~/.aws/credentials)
+
+dotnet run --no-launch-profile --project ./SiloHost/SiloHost.csproj
