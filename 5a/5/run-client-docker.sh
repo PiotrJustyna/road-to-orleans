6da586193ba70ba3b045a489285dee4229badbcd
André Silva <2493377+askpt@users.noreply.github.com>
2022-01-25 15:53:22 +0000

    Copied solution

---
+++ 5a/5/run-client-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 2020-12-07 PJ:
+# Code below gets local IP for dockerized applications to function and communicate correctly.
+ADVERTISEDIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v -m1 '127.0.0.1'`
+GATEWAYPORT=3001
+
+docker build -t client-cluster -f ./ops/Client/Dockerfile ./ &&
+  docker run -it -e ADVERTISEDIP=$ADVERTISEDIP -e GATEWAYPORT=$GATEWAYPORT --rm client-cluster
