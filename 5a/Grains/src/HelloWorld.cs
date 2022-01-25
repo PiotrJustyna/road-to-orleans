6b55fe2e20232b1b05e9298b5d7649e9e9f43177
André Silva <2493377+askpt@users.noreply.github.com>
2022-01-25 16:10:16 +0000

    Add fsharp host support (#304)
    Added 5a.

---
+++ 5a/Grains/src/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Interfaces;
+using Library;
+
+namespace Grains
+{
+    public class HelloWorld : Orleans.Grain, IHelloWorld
+    {
+        public Task<string> SayHello(string name)
+        {
+            return Task.FromResult(Say.hello(name));
+        }
+    }
+}
