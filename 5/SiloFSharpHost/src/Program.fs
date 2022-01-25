ba720632e7997f56ffa2c40e0c394a098bdc197d
André Silva <2493377+askpt@users.noreply.github.com>
2022-01-25 15:36:38 +0000

    Added the initial ipaddress variable.

--- 5/SiloFSharpHost/src/Program.fs
+++ 5/SiloFSharpHost/src/Program.fs
@@ -1,15 +1,54 @@
 open System
-open Microsoft.AspNetCore.Builder
-open Microsoft.Extensions.Hosting
+open System.Net
+open System.Net.NetworkInformation
+open System.Net.Sockets
+
+let localIpAddress () : Async<IPAddress> =
+    async {
+        let networkInterfaces =
+            NetworkInterface.GetAllNetworkInterfaces()
+            |> Array.filter (fun ni -> ni.OperationalStatus.Equals(OperationalStatus.Up))
+
+        let addresses =
+            seq {
+                for ni in networkInterfaces do
+                    for unicastAddress in ni.GetIPProperties().UnicastAddresses do
+                        yield unicastAddress.Address
+            }
+
+        return
+            addresses
+            |> Seq.filter
+                (fun address ->
+                    address.AddressFamily.Equals(AddressFamily.InterNetwork)
+                    && not (IPAddress.IsLoopback(address)))
+            |> Seq.head
+    }
+
+let ipAddress () : Async<IPAddress> =
+    async {
+        let parsed, environmentIp =
+            Environment.GetEnvironmentVariable("ADVERTISEDIP")
+            |> IPAddress.TryParse
+
+        return!
+            async {
+                match parsed with
+                | true -> return environmentIp
+                | false -> return! localIpAddress ()
+            }
+    }
 
 [<EntryPoint>]
 let main args =
-    let builder = WebApplication.CreateBuilder(args)
-    let app = builder.Build()
+    //    let builder = WebApplication.CreateBuilder(args)
+//    let app = builder.Build()
+//
+//    app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
+//
+//    app.Run()
+//
+//    0 // Exit code
+    printfn $"{ipAddress ()}"
 
-    app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
-
-    app.Run()
-
-    0 // Exit code
-
+    0
