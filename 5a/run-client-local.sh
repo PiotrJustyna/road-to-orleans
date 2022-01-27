9d298c3b6835ae4c7f6e41e422070ea7dd4fb49c
André Silva <2493377+askpt@users.noreply.github.com>
2022-01-27 10:36:35 +0000

    Convert client project (#307)

--- 5a/run-client-local.sh
+++ 5a/run-client-local.sh
@@ -2,4 +2,4 @@
 export ADVERTISEDIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v -m1 '127.0.0.1'`
 export GATEWAYPORT=3001
 
-dotnet run --project ./Client/Client.csproj
+dotnet run --project ./Client/Client.fsproj
