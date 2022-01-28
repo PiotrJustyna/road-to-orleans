f2f9c99d2a49a31fa0ef135c4e0e57528067e1f8
Amedeo <20638889+AmedeoV@users.noreply.github.com>
2022-01-28 10:56:57 +0000

    Rewriting the Silo code in fsharp (#308)

--- 5a/SiloHost/src/Program.fs
+++ 5a/SiloHost/src/Program.fs
@@ -2,6 +2,14 @@ open System
 open System.Net
 open System.Net.NetworkInformation
 open System.Net.Sockets
+open Grains
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+open Orleans.Configuration
+open Orleans.Hosting
+open Orleans.Statistics
+open Orleans
+open OrleansDashboard
 
 let localIpAddress () : Async<IPAddress> =
     async {
@@ -116,8 +124,35 @@ let main args =
     printfn $"Gateway Port: {gatewayPort}"
     printfn $"Primary Silo Port: {primarySiloPort}"
     printfn $"Dashboard Port: {dashboardPort}"
+    
+    let configureLogging (builder : ILoggingBuilder) =
+        let filter (l : LogLevel) = l.Equals LogLevel.Information
+        builder.AddFilter(filter).AddConsole().AddDebug() |> ignore
 
-    //TODO Add Orleans host
-
+    let builder = HostBuilder()
+    let siloConfiguration =
+        fun (siloBuilder: ISiloBuilder) ->
+            siloBuilder
+                .UseDevelopmentClustering(fun (options: DevelopmentClusterMembershipOptions) -> options.PrimarySiloEndpoint <- IPEndPoint(ipAddress, primarySiloPort))
+                .Configure<ClusterOptions>(fun (options: ClusterOptions) ->
+                    options.ClusterId <- "cluster-of-silos"
+                    options.ServiceId <- "hello-world-service")
+                .Configure<EndpointOptions>(fun (options: EndpointOptions) ->
+                    options.SiloPort <- siloPort
+                    options.AdvertisedIPAddress <- ipAddress
+                    options.GatewayPort <- gatewayPort
+                    options.SiloListeningEndpoint <- IPEndPoint(IPAddress.Any, siloPort)
+                    options.GatewayListeningEndpoint <- IPEndPoint(IPAddress.Any, gatewayPort))
+                .UseDashboard(fun (options: DashboardOptions) ->
+                    options.Username <- "piotr"
+                    options.Password <- "orleans"
+                    options.Port <- dashboardPort)
+                .UseLinuxEnvironmentStatistics()
+                .ConfigureApplicationParts(fun applicationPartManager -> applicationPartManager.AddApplicationPart(typeof<HelloWorld>.Assembly).WithReferences() |> ignore)
+                .ConfigureLogging(configureLogging) |> ignore
+    
+    builder.UseOrleans(siloConfiguration).RunConsoleAsync()
+    |> Async.AwaitTask
+    |> Async.RunSynchronously
 
     0
