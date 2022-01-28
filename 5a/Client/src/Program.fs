4ba2fdfb312c33258935cdfb7abc2985316ec0bb
André Silva <2493377+askpt@users.noreply.github.com>
2022-01-28 10:06:24 +0000

    Added Client initial configuration.

--- 5a/Client/src/Program.fs
+++ 5a/Client/src/Program.fs
@@ -1,3 +1,13 @@
 module Client
 
-printfn "Hello World!"
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+
+printfn "Starting Client"
+
+HostBuilder()
+    .ConfigureServices(fun (services: IServiceCollection) ->
+        services.Configure<ConsoleLifetimeOptions>(fun (options: ConsoleLifetimeOptions) -> options.SuppressStatusMessages <- true) |> ignore
+        ).ConfigureLogging(fun (builder: ILoggingBuilder) -> builder.AddConsole() |> ignore)
+         .RunConsoleAsync() |> ignore
