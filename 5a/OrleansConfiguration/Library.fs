317bd348cc240b0bacb582e2d83115293eec97b7
André Silva <2493377+askpt@users.noreply.github.com>
2022-02-01 10:46:19 +0000

    Formatting! :)

--- 5a/OrleansConfiguration/Library.fs
+++ 5a/OrleansConfiguration/Library.fs
@@ -7,16 +7,16 @@ open System.Net.Sockets
 
 module Ports =
     let port (environmentVariableKey: string) : Async<int> =
-        async {
-            let parsed, port =
-                Environment.GetEnvironmentVariable(environmentVariableKey)
-                |> Int32.TryParse
+        let parsed, parsedPort =
+            Environment.GetEnvironmentVariable(environmentVariableKey)
+            |> Int32.TryParse
 
-            return
-                match parsed with
-                | true -> port
-                | false -> raise (ArgumentException($"${environmentVariableKey} environment variable not set"))
-        }
+        let port =
+            match parsed with
+            | true -> parsedPort
+            | false -> raise (ArgumentException($"${environmentVariableKey} environment variable not set"))
+
+        async { return port }
 
     let gatewayPort () : Async<int> = async { return! port "GATEWAYPORT" }
 
@@ -28,37 +28,37 @@ module Ports =
 
 module IpAddresses =
     let localIpAddress () : Async<IPAddress> =
-        async {
-            let networkInterfaces =
-                NetworkInterface.GetAllNetworkInterfaces()
-                |> Array.filter (fun ni -> ni.OperationalStatus.Equals(OperationalStatus.Up))
+        let networkInterfaces =
+            NetworkInterface.GetAllNetworkInterfaces()
+            |> Array.filter (fun ni -> ni.OperationalStatus.Equals(OperationalStatus.Up))
 
-            let addresses =
-                seq {
-                    for ni in networkInterfaces do
-                        for unicastAddress in ni.GetIPProperties().UnicastAddresses do
-                            yield unicastAddress.Address
-                }
+        let addresses =
+            seq {
+                for ni in networkInterfaces do
+                    for unicastAddress in ni.GetIPProperties().UnicastAddresses do
+                        yield unicastAddress.Address
+            }
 
-            return
-                addresses
-                |> Seq.filter
-                    (fun address ->
-                        address.AddressFamily.Equals(AddressFamily.InterNetwork)
-                        && not (IPAddress.IsLoopback(address)))
-                |> Seq.head
-        }
+        let address =
+            addresses
+            |> Seq.filter
+                (fun address ->
+                    address.AddressFamily.Equals(AddressFamily.InterNetwork)
+                    && not (IPAddress.IsLoopback(address)))
+            |> Seq.head
+
+        async { return address }
 
     let advertisedIpAddress () : Async<IPAddress> =
-        async {
-            let parsed, environmentIp =
-                Environment.GetEnvironmentVariable("ADVERTISEDIP")
-                |> IPAddress.TryParse
+        let parsed, environmentIp =
+            Environment.GetEnvironmentVariable("ADVERTISEDIP")
+            |> IPAddress.TryParse
 
-            return!
-                async {
-                    match parsed with
-                    | true -> return environmentIp
-                    | false -> return! localIpAddress ()
-                }
-        }
+        let ip =
+            async {
+                match parsed with
+                | true -> return environmentIp
+                | false -> return! localIpAddress ()
+            }
+
+        async { return! ip }
