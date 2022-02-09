f7ee664b79c026f70c6613b4b5e0e32bad6e0809
André Silva <2493377+askpt@users.noreply.github.com>
2022-02-09 09:37:58 +0000

    Changed to the new main.

--- 3c/SiloHost/src/Program.cs
+++ 3c/SiloHost/src/Program.cs
@@ -9,81 +9,79 @@ using Orleans;
 using Orleans.Configuration;
 using Orleans.Hosting;
 using Orleans.Statistics;
+using SiloHost;
 
-namespace SiloHost
-{
-    class Program
+var advertisedIp = Environment.GetEnvironmentVariable("ADVERTISEDIP");
+var advertisedIpAddress = advertisedIp == null ? GetLocalIpAddress() : IPAddress.Parse(advertisedIp);
+var gatewayPort = int.Parse(Environment.GetEnvironmentVariable("GATEWAYPORT") ?? "3000");
+var siloEndpointConfiguration = GetSiloEndpointConfiguration(advertisedIpAddress, gatewayPort);
+
+// Configure web host
+var builder = Host.CreateDefaultBuilder()
+    .ConfigureWebHostDefaults(webHostBuilder =>
     {
-        public static async Task Main()
+        webHostBuilder
+            .UseStartup<Startup>()
+            .UseUrls("http://*:5000");
+    });
+
+// Configure orleans
+builder.UseOrleans(siloBuilder =>
+{
+    siloBuilder.UseLinuxEnvironmentStatistics();
+    siloBuilder.UseDashboard(dashboardOptions =>
+    {
+        dashboardOptions.Username = "piotr";
+        dashboardOptions.Password = "orleans";
+    });
+    siloBuilder.UseLocalhostClustering();
+    siloBuilder.Configure<EndpointOptions>(endpointOptions =>
+    {
+        endpointOptions.AdvertisedIPAddress = siloEndpointConfiguration.Ip;
+        endpointOptions.SiloPort = siloEndpointConfiguration.SiloPort;
+        endpointOptions.GatewayPort = siloEndpointConfiguration.GatewayPort;
+        endpointOptions.SiloListeningEndpoint = new IPEndPoint(IPAddress.Any, 2000);
+        endpointOptions.GatewayListeningEndpoint =
+            new IPEndPoint(IPAddress.Any, siloEndpointConfiguration.GatewayPort);
+    });
+    siloBuilder.ConfigureApplicationParts(applicationPartManager =>
+        applicationPartManager.AddApplicationPart(typeof(HelloWorld).Assembly).WithReferences());
+});
+
+// Configure logging
+builder.ConfigureLogging(logging => logging.AddConsole());
+
+// Run host
+await builder.RunConsoleAsync();
+
+static SiloEndpointConfiguration GetSiloEndpointConfiguration(
+    IPAddress advertisedAddress,
+    int gatewayPort)
+{
+    return new(advertisedAddress, 2000, gatewayPort);
+}
+
+static IPAddress GetLocalIpAddress()
+{
+    var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+    foreach (var network in networkInterfaces)
+    {
+        if (network.OperationalStatus != OperationalStatus.Up)
+            continue;
+
+        var properties = network.GetIPProperties();
+        if (properties.GatewayAddresses.Count == 0)
+            continue;
+
+        foreach (var address in properties.UnicastAddresses)
         {
-            var advertisedIp = Environment.GetEnvironmentVariable("ADVERTISEDIP");
-            var advertisedIpAddress = advertisedIp == null ? GetLocalIpAddress() : IPAddress.Parse(advertisedIp);
-            var gatewayPort = int.Parse(Environment.GetEnvironmentVariable("GATEWAYPORT") ?? "3000");
-
-            var siloEndpointConfiguration = GetSiloEndpointConfiguration(advertisedIpAddress, gatewayPort);
-
-            await Host.CreateDefaultBuilder()
-                .ConfigureWebHostDefaults(webHostBuilder =>
-                {
-                    webHostBuilder
-                        .UseStartup<Startup>()
-                        .UseUrls("http://*:5000");
-                })
-                .UseOrleans(siloBuilder =>
-                {
-                    siloBuilder.UseLinuxEnvironmentStatistics();
-                    siloBuilder.UseDashboard(dashboardOptions =>
-                    {
-                        dashboardOptions.Username = "piotr";
-                        dashboardOptions.Password = "orleans";
-                    });
-                    siloBuilder.UseLocalhostClustering();
-                    siloBuilder.Configure<EndpointOptions>(endpointOptions =>
-                    {
-                        endpointOptions.AdvertisedIPAddress = siloEndpointConfiguration.Ip;
-                        endpointOptions.SiloPort = siloEndpointConfiguration.SiloPort;
-                        endpointOptions.GatewayPort = siloEndpointConfiguration.GatewayPort;
-                        endpointOptions.SiloListeningEndpoint = new IPEndPoint(IPAddress.Any, 2000);
-                        endpointOptions.GatewayListeningEndpoint =
-                            new IPEndPoint(IPAddress.Any, siloEndpointConfiguration.GatewayPort);
-                    });
-                    siloBuilder.ConfigureApplicationParts(applicationPartManager =>
-                        applicationPartManager.AddApplicationPart(typeof(HelloWorld).Assembly).WithReferences());
-                })
-                .ConfigureLogging(logging => logging.AddConsole())
-                .RunConsoleAsync();
-        }
-
-        private static SiloEndpointConfiguration GetSiloEndpointConfiguration(
-            IPAddress advertisedAddress,
-            int gatewayPort)
-        {
-            return new(advertisedAddress, 2000, gatewayPort);
-        }
-
-        private static IPAddress GetLocalIpAddress()
-        {
-            var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
-            foreach (var network in networkInterfaces)
+            if (address.Address.AddressFamily == AddressFamily.InterNetwork &&
+                !IPAddress.IsLoopback(address.Address))
             {
-                if (network.OperationalStatus != OperationalStatus.Up)
-                    continue;
-
-                var properties = network.GetIPProperties();
-                if (properties.GatewayAddresses.Count == 0)
-                    continue;
-
-                foreach (var address in properties.UnicastAddresses)
-                {
-                    if (address.Address.AddressFamily == AddressFamily.InterNetwork &&
-                        !IPAddress.IsLoopback(address.Address))
-                    {
-                        return address.Address;
-                    }
-                }
+                return address.Address;
             }
-
-            return null;
         }
     }
+
+    return null;
 }
